5c67b0e2f3d1e7a057fd96d026647c2f03fd91e4
wwxi1 <420345378@qq.com>
2026-08-30 15:24:18 +0800

    把主要can路改到can2

--- Motor/Inc/motor_config.h
+++ Motor/Inc/motor_config.h
@@ -58,8 +58,8 @@ extern "C"
 /* ------------------------------------------------------------------ */
 #define MOTOR_ZDRIVE_COUNT 6U       /* 最多控 8 个电机 */
 #define MOTOR_ZDRIVE_SPLIT_COUNT 4U /* 0=不拆分;n=前 n 个 ID 走第一路 */
-#define MOTOR_ZDRIVE_CAN_BUS_1 1U   /* 第一路:CAN2 */
-#define MOTOR_ZDRIVE_CAN_BUS_2 1U   /* 第二路:CAN2(F4 无 CAN3) */
+#define MOTOR_ZDRIVE_CAN_BUS_1 0U   /* 第一路:CAN2 */
+#define MOTOR_ZDRIVE_CAN_BUS_2 0U   /* 第二路:CAN2(F4 无 CAN3) */
 #define MOTOR_ZDRIVE_BUS_RETRANS_CNT 2 //调用出队函数时,单BUS连续发送的次数
 
 
