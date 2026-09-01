a581012f6abd1e2e2e6c0b30d229e6dafe6f5e05
wwxi1 <420345378@qq.com>
2026-09-01 09:40:03 +0800

    把CAN通信改为can2

--- IRQ/Src/CAN_IRQHandler.c
+++ IRQ/Src/CAN_IRQHandler.c
@@ -20,9 +20,7 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
         // 从 FIFO 0 把数据捞出来，存到 RxData 数组里
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
         {
-            #if USE_DJ && (MOTOR_DJI_CAN_BUS == 0U)
-            DJmotor_Receive(RxHeader, RxData);
-            #endif
+           //
         }
     }
     else if (hcan->Instance == CAN2)
@@ -30,7 +28,11 @@ void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *hcan)
 
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData) == HAL_OK)
         {
-            //
+            
+            #if USE_DJ && (MOTOR_DJI_CAN_BUS == 1U)
+            DJmotor_Receive(RxHeader, RxData);
+            #endif
+
         }
     }
 }
@@ -44,16 +46,17 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
     {
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, RxData) == HAL_OK)
         {
-            Ground_Block_Func(RxHeader, RxData);
+            #if USE_ZMDR
+            ZdriveReceive(RxHeader, RxData, 1U);
+            #endif
         }
+        
     }
     else if (hcan->Instance == CAN2)
     {
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, RxData) == HAL_OK)
         {
-           #if USE_ZMDR
-            ZdriveReceive(RxHeader, RxData, 1U);
-            #endif
+           Ground_Block_Func(RxHeader, RxData);
         }
     }
 }
