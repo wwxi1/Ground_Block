e6bbf7d21eefb00f75a6b179a9f04799c3628d5f
wwxi1 <420345378@qq.com>
2026-09-01 19:47:56 +0800

    两个can的过滤器相同，再次更改can回调为can1大疆，can2通信

--- IRQ/Src/CAN_IRQHandler.c
+++ IRQ/Src/CAN_IRQHandler.c
@@ -44,7 +44,9 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
     {
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, RxData) == HAL_OK)
         {
-            Ground_Block_Func(RxHeader, RxData);
+            #if USE_ZMDR
+            ZdriveReceive(RxHeader, RxData, 1U);
+            #endif
         }
         
     }
@@ -52,9 +54,7 @@ void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
     {
         if (HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO1, &RxHeader, RxData) == HAL_OK)
         {
-           #if USE_ZMDR
-            ZdriveReceive(RxHeader, RxData, 1U);
-            #endif
+           Ground_Block_Func(RxHeader, RxData);
         }
     }
 }
